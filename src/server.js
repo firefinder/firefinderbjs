8b435205a28e41598c4d111d8f73003a9058f529
flamelessdev <flamelessdevs@gmail.com>
2020-07-10 22:40:01 +0930

    tracked

---
+++ src/server.js
@@ -0,0 +1,232 @@
+const express = require('express');
+const { connect } = require('mongoose');
+const KeyModel = require('../models/Key');
+const FireModel = require('../models/Fire');
+const moment = require('moment');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    return res.status(200).json({
+        "message": "FireAPI - Powering FireApp since 2020"
+    })
+})
+
+app.get('/fires', async (req, res) => {
+    let auth = req.headers.authorization;
+    if(!auth) {
+        console.log('no auth')
+        res.status(401).send()
+        return
+    }
+    if(!await KeyModel.findOne({ key:auth })) {
+        console.log('incorrect auth')
+        res.status(401).send()
+        return
+    }
+    
+    let code = req.query.locCode;
+    if(!code) {
+        res.status(400).json({
+            "message": "Please provide a location code (locCode parameter)"
+        }).send()
+        return;
+    }
+    if(isNaN(code)) {
+        res.status(400).json({
+            "message": "Location codes must be presented in integer format."
+        }).send()
+        return;
+    }
+    let locationFind = await FireModel.findOne({ locCode: code });
+    if(!locationFind) {
+        res.status(200).json({
+            "message": "You provided an invalid location code, generally meaning you made a mistake in the request or there are no active fires in the area."
+        }).send()
+        return;
+    } else {
+        let activeFires = locationFind.fires.filter(object => object.active === true);
+        if(activeFires.length < 1) {
+            res.status(200).json({
+                "message": "There are no active fires for this area."
+            }).send()
+            return;
+        } else {
+            res.status(200).json(activeFires).send();
+            return;
+        }
+    }
+})
+
+app.post('/fires', async (req, res) => {
+    let auth = req.headers.authorization;
+    if(!auth) return res.status(401).send();
+    let dbEntry = await KeyModel.findOne({ key:auth });
+    if(!dbEntry) return res.status(401).send();
+    if(dbEntry.authLevel < 1) return res.status(401).send()
+    
+    let json = req.body;
+    let severity = json.severity;
+    let locCode = json.locCode;
+    let description = json.description;
+
+    if(!severity || !locCode || !description) {
+        res.status(400).send();
+        return;
+    } else if(typeof locCode !== 'number' || typeof severity !== 'number' || typeof description !== 'string') {
+        res.status(400).send();
+        return;
+    }
+
+    let FireModelForLoc = await FireModel.findOne({ locCode: locCode });
+    if(!FireModelForLoc) {
+        let newModel = new FireModel({ 
+            locCode: locCode,
+            fires: [{ 
+                id: 0,
+                description: description,
+                severity: severity,
+                active: true,
+                timestamp: Date.now(),
+                humanFriendlyTimestamp: moment().format("HH:mma ddd D/M/YY"),
+                instantiatedBy: auth 
+            }]
+        });
+        await newModel.save().catch(() => {
+            res.status(500).send();
+            return;
+        })
+        return res.status(201).json({
+            "message": `Fire created in location code ${locCode} with ID ${parseInt('0')}.`
+        }).send()
+    } else {
+        let idNo;
+        try {
+            idNo = Number(FireModelForLoc.fires[FireModelForLoc.fires.length - 1].id + 1);
+        } catch (error) {
+            idNo = undefined
+            return res.status(500).json({
+                "stack":error
+            })
+        }
+        FireModelForLoc.fires.push({
+            id: idNo,
+            description: description, 
+            severity: severity, 
+            active: true, 
+            timestamp: Date.now(), 
+            humanFriendlyTimestamp: moment().format("h:mma ddd D/M/YY"), 
+            instantiatedBy: auth
+        })
+        await FireModelForLoc.save().catch(() => {
+            res.status(500).send();
+            return;
+        })
+        return res.status(201).json({
+            "message": `Fire created in location code ${locCode} with ID ${idNo}.`
+        }).send()
+    }
+})
+
+app.delete('/fires', async (req, res) => {
+    let auth = req.headers.authorization;
+    if(!auth) return res.status(401).send();
+    let dbEntry = await KeyModel.findOne({ key:auth });
+    if(!dbEntry) return res.status(401).send();
+    if(dbEntry.authLevel < 1) return res.status(401).send()
+
+    let locCode = req.query.locCode;
+    if(!locCode) {
+        return res.status(400).json({
+            "message": "Provide a location code to delete."
+        }).send()
+    }
+    let dbLoc = await FireModel.findOne({ locCode: locCode })
+    if(!dbLoc) {
+        return res.status(404).json({
+            "message": "This location code does not exist in the database. \nThis generally means that either the code is invalid or no fires have ever occurred in the area."
+        }).send();
+    } else {
+        await dbLoc.deleteOne().catch(err => {
+            console.error(err)
+            res.status(500).send();
+        });
+        return res.status(200).send();
+    }
+})
+
+app.patch('/fires', async (req, res) => {
+    let auth = req.headers.authorization;
+    if(!auth) return res.status(401).send();
+    auth = await KeyModel.findOne({ key: auth });
+    if(!auth) return res.status(401).send();
+    if(auth.authLevel < 1) return res.status(401).send();
+
+    let fireLoc = parseInt(req.query.locCode)
+    let fireID = parseInt(req.query.id)
+    let action = req.query.action
+
+    if(!fireLoc || !fireID) return res.status(400).send();
+    let locationDBEntry = await FireModel.findOne({ locCode: fireLoc });
+    if(!locationDBEntry) return res.status(400).json({
+        "message": "The provided fire location does not exist in the database."
+    }).send();
+    let specificFire = locationDBEntry.fires.find(element => element.id === fireID);
+    if(!specificFire) return res.status(400).json({
+        "message": "The specified fire ID does not exist in the database."
+    }).send();
+
+    switch (action) {
+        case 'disable': {
+            try {
+                // await locationDBEntry.updateOne({ ['fires[' + Number(fireID - 1) + '].active']:false });
+                locationDBEntry.fires[Number(fireID - 1)].active = false;
+                locationDBEntry.save().catch((err) => {
+                    res.status(500).json({
+                        "error":err
+                    }).send()
+                })
+            } catch (error) {
+                return res.status(500).json({
+                    "error": error
+                })
+            }
+            res.status(200).send()
+        }
+        break;
+        case 'enable': {
+            try {
+                locationDBEntry.fires[Number(fireID - 1)].active = true;
+                locationDBEntry.save().catch((err) => {
+                    return res.status(500).json({
+                        "error":err
+                    }).send()
+                })
+            } catch (error) {
+                return res.status(500).send({
+                    "error":error
+                })
+            }
+            res.status(200).send();
+        }
+        break;
+        default: {
+            return res.status(400).send();
+        }
+    }
+})
+
+app.listen(process.env.PORT);
+
+(async () => {
+    await connect(process.env.MONGODB_URI, {
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useNewUrlParser: true
+    })
+    console.log('DB Logged in.')
+})()
